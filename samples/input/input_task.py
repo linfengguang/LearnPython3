5ae17508ea2436ad16b97291fa22d99b504cf977
linfengguang <453093194@qq.com>
2017-10-13 17:26:57 +0800

    commit the task of "print&input"

--- samples/input/input_task.py
+++ samples/input/input_task.py
@@ -1,2 +1,8 @@
 # -*- coding:utf-8 -*-
 
+print('please input the first num:')
+num1 = int(input())
+print('please input the second num:')
+num2 = int(input())
+print('the product is:')
+print('%d * %d = '%(num1, num2), num1*num2)
