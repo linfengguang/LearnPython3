db9808d3671a3577546f40deb00e6371fa0b4468
linfengguang <453093194@qq.com>
2017-10-16 14:30:08 +0800

    commit the task of "var"

---
+++ samples/basic/var_task.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+n = 123
+f = 456.789
+s1 = 'hello,world'
+s2 = 'hello,\'Adam\''
+s3 = r'Hello,"Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+print('%d\n%d\n%s\n%s\n%s\n%s' % (n, f, s1, s2, s3, s4))
