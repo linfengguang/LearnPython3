501d8542bec5e39752bd5b3af7d5ad1b57663bed
linfengguang <453093194@qq.com>
2017-10-17 14:33:59 +0800

    commit the eg&task of "loop"

---
+++ samples/basic/loop_eg.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+sum1 = 0
+n = 1
+while n < 100:
+    sum1 = sum1 + n
+    n = n + 2
+print(sum1)
---
+++ samples/basic/loop_task.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+'''
+请利用循环依次对list中的每个名字打印出Hello, xxx!
+'''
+
+L = ['Lisa', 'Tim', 'Adam']
+for name in L:
+    print('Hello, %s !' % name)
