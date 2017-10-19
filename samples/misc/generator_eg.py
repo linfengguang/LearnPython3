0707231ce16c1a17319016ce316d1d2227ac193a
linfengguang <453093194@qq.com>
2017-10-19 10:30:34 +0800

    commit the eg of "generator"

---
+++ samples/misc/generator_eg.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b , a + b
+        n = n + 1
+    return 'done'
+
+f = fib(10)
+while True:
+    try:
+        x = next(f)
+        print(x)
+    except StopIteration as e:
+        print(e.value)
+        break
