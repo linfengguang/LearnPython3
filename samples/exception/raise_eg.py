49fab876b39c7ec6dff82a326e7150081026a4a9
linfengguang <453093194@qq.com>
2017-10-23 11:22:07 +0800

    commit the eg of "raise logging"

---
+++ samples/exception/raise_eg.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n == 0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+print(foo('haha'))
